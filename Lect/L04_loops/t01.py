12d762b19dc9258700156725042dd2dc721c004d
Andrii Krenevych <akrenevych@gmail.com>
2023-09-19 12:22:06 +0300

    Лекція4 (цикли)

---
+++ Lect/L04_loops/t01.py
@@ -0,0 +1,13 @@
+# вивести 10 разів фразу "Hello World!"
+
+n = int(input("Скільки тобі разів вивести 'Hello World!'? "))
+
+i = 1
+while i < n + 1:
+    print(f"{i}: Hello World!")
+    # i += 1
+
+# for i in range(1, n+1):
+#     print(f"Hello World!")
+
+pass
