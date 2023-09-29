ee94976e3d579ca21b51510771e0ed5ba0f6eb67
Andrii Krenevych <krenevych@knu.ua>
2023-09-29 14:57:47 +0300

    111

--- KompMat_2/L07/t02.py
+++ KompMat_2/L07/t02.py
@@ -13,5 +13,11 @@ for i in range(10, 100):
         pass
     elif n == 3:
         pass
+    elif n == 5:
+#         Обчислити суму
+#         двозначних чисел, цифри яких парні.
+        if od % 2 == 0 and de % 2 == 0:
+            counter += i
+
 
 print(counter)
