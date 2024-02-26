92b0203414a0a352412daee9915be6aa6f9f4bbe
Andrii Krenevych <krenevych@knu.ua>
2024-02-26 08:47:28 +0200

    Triangle - L03

---
+++ Lect_OOP/L03/Triangle.py
@@ -0,0 +1,23 @@
+import copy
+class Triangle:
+    def __init__(self, a, b, c):
+
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def perimeter(self):
+        return (self.a + self.b + self.c) / 2
+
+    def area(self):
+        p = self.perimeter()
+        return (p * (p - self.a) * (p - self.b) * (p - self.c))**0.5
+
+    def show(self):
+        print("Triangle", self.a, self.b, self.c)
+
+
+if __name__ == '__main__':
+    t = Triangle(3, 4, 5)
+    t.show()
+
