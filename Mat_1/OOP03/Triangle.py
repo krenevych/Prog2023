2f932fc032c9c314d0708cada5653b5719c8f124
Andrii Krenevych <krenevych@knu.ua>
2024-02-27 12:08:19 +0200

    triangles: colors

--- Mat_1/OOP03/Triangle.py
+++ Mat_1/OOP03/Triangle.py
@@ -1,13 +1,12 @@
 from turtle import *
 from random import randint
-
-
 class Triangle:
 
     def __init__(self, x1, y1, x2, y2):
         self._vertex1 = (x1, y1)
         self._vertex2 = (x2, y2)
         self._position = (0, 0)
+        self._color = "black"
 
     def translate(self, dx, dy):
         self._position = (self._position[0] + dx,
@@ -16,17 +15,20 @@ class Triangle:
     def set_position(self, x, y):
         self._position = (x, y)
 
+    def set_color(self, color):
+        self._color = color
+
     def calc_absolute_position(self, vertex):
         x = self._position[0] + vertex[0]
         y = self._position[1] + vertex[1]
         return x, y
-
 
     def draw(self):
         v0 = self.calc_absolute_position((0, 0))
         v1 = self.calc_absolute_position(self._vertex1)
         v2 = self.calc_absolute_position(self._vertex2)
         up()
+        pencolor(self._color)
         setpos(*v0)
         down()
         goto(*v1)
@@ -36,6 +38,16 @@ class Triangle:
 
 def createTriangles(number):
     # генерація випадкового числа з проміжку  [0, 10]
+    colors = [
+        "green yellow",
+        "teal",
+        "deep pink",
+        "rosy brown",
+        "red",
+        "green",
+        "black",
+    ]
+
     bound = 350
     triangles = []
     for i in range(number):
@@ -44,11 +56,13 @@ def createTriangles(number):
         x2 = randint(-bound, bound)
         y2 = randint(-bound, bound)
         t = Triangle(x1, y1, x2, y2)
+        color_num = randint(0, len(colors) - 1)
+        t.set_color(colors[color_num])
         triangles.append(t)
     return triangles
 
 if __name__ == '__main__':
-    triangles = createTriangles(5)
+    triangles = createTriangles(10)
     for t in triangles:
         bound = 100
         x0 = randint(-bound, bound)
