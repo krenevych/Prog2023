7b7fd228357147ea769005ebf3eb36c8d4c3cd3b
Andrii Krenevych <krenevych@knu.ua>
2024-03-05 11:10:51 +0200

    Equation

---
+++ Mat_1/OOP04/Equation.py
@@ -0,0 +1,28 @@
+class Equation:
+    """
+    клас Equation (Рівняння), що моделює лінійне алгебраїчне рівняння виду
+                    bx+c=0.
+    """
+    INF = "infinity"
+
+    def __init__(self, b, c):
+        self.b = b
+        self.c = c
+
+    def solve(self):
+        if self.b != 0:
+            x1 = -self.c / self.b
+            return (x1, )
+        else: # b == 0
+            if self.c == 0: # 0 == 0
+                return Equation.INF
+            else: # 0 == 10
+                return ()
+
+if __name__ == '__main__':
+    eq1 = Equation(2, 3)
+    print(eq1.solve())
+    eq2 = Equation(0, 0)
+    print(eq2.solve())
+    eq3 = Equation(0, 5)
+    print(eq3.solve())
