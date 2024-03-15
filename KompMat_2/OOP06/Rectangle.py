d21b0a5b6ef11b9ccbf11aed5cd4244037fb20ef
Andrii Krenevych <krenevych@knu.ua>
2024-03-15 11:15:13 +0200

    Rectangle

--- KompMat_2/OOP06/Rectangle.py
+++ KompMat_2/OOP06/Rectangle.py
@@ -1,35 +1,35 @@
 from Figure import Figure
 
-from math import sin, cos, pi, radians
 import turtle
 
-class Triangle(Figure):
+class Rectangle(Figure):
 
-    def __init__(self, x1, y1, x2, y2):
+    def __init__(self, x1, y1):
         super().__init__()
         self._vertex1 = (x1, y1)
-        self._vertex2 = (x2, y2)
 
     def draw(self):
-        v1 = self.calc_abs_pos(self._vertex1)
-        v2 = self.calc_abs_pos(self._vertex2)
+        v1 = self.calc_abs_pos((self._vertex1[0], 0))
+        v2 = self.calc_abs_pos(self._vertex1)
+        v3 = self.calc_abs_pos((0, self._vertex1[1]))
         turtle.color(self.color)
         turtle.up()
         turtle.setpos(*self.position)
         turtle.down()
         turtle.goto(*v1)
         turtle.goto(*v2)
+        turtle.goto(*v3)
         turtle.setpos(*self.position)
         turtle.up()
-        turtle.color(Triangle.default_color)
+        turtle.color(Rectangle.default_color)
 
 
 
 if __name__ == '__main__':
     turtle.speed(0)
-    triangle = Triangle(100, 100, 100, 0)
+    triangle = Rectangle(100, 100)
     triangle.draw()
-    # turtle.clear()
+
     for degree in range(3, 363, 3):
         triangle.set_rotation_degree(degree)
 
